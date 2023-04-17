f5f4e30d0d76758d566b24e80e6e6e9f41021150
Alibek Beket <alibek1beket@gmail.com>
2023-04-18 01:45:39 +0600

    добавил фукнцию для изменения задачи, создал путь и удалил лишниии функции в модели 'IssueSerializer'

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.views import IssueDetailView
+from api.views import IssueDetailView, IssueUpdateView
 
 urlpatterns = [
     path('detail/<int:pk>/', IssueDetailView.as_view(), name='api_issue_detail'),
+    path('update/<int:pk>/', IssueUpdateView.as_view(), name='api_issue_update'),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -10,3 +10,13 @@ class IssueDetailView(APIView):
         objects = Issue.objects.filter(id=self.kwargs['pk'])
         serializer = IssueSerializer(objects, many=True)
         return Response(serializer.data)
+
+
+class IssueUpdateView(APIView):
+    def put(self, request, *args, **kwargs):
+        objects = Issue.objects.filter(id=self.kwargs['pk']).first()
+        serializer = IssueSerializer(objects, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
