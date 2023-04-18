e7d20c69f59ee997a93bdc9c04c188e3e342f579
Alibek Beket <alibek1beket@gmail.com>
2023-04-18 17:08:13 +0600

    добавил в field(IssueSerializer, ProjectSerializer) значение id

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -5,12 +5,12 @@ from issue_tracker.models import Issue, Project
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
-        fields = ('summary', 'description', 'status', 'created_at', 'updated_at', 'type', 'project')
+        fields = ('id', 'summary', 'description', 'status', 'created_at', 'updated_at', 'type', 'project')
         read_only_fields = ('id', 'created_at', 'updated_at')
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('name', 'description', 'start_date', 'end_date', 'is_deleted', 'user')
+        fields = ('id', 'name', 'description', 'start_date', 'end_date', 'is_deleted', 'user')
         read_only_fields = ('id', 'is_deleted')
